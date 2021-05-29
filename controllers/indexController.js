fbc7fd4748566828fcb3d724ed903579390bfb23
nei <nei.thomass@gmail.com>
2021-05-29 14:52:34 -0300

    desenvolvido formulario de email para contato

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -9,13 +9,13 @@ exports.contato_get = (req, res, next) => {
 exports.contato_post = (req, res, next) => {
 
     let c_name = req.body.contato_name;
-    let c_mail = req.body.contato_mail;
+    let c_mail = req.body.contato_email;
     let c_title = req.body.contato_title;
     let c_message = req.body.contato_message;
 
     // config email transport
     const transporter = nodemailer.createTransport({
-      port: process.env.MAIL_PORT, // true for 465, false for other ports
+      port: process.env.MAIL_PORT, // true for 465, false for other ports(587)
       host: process.env.MAIL_HOST,
          auth: {
               user: process.env.MAIL_USER,
@@ -28,8 +28,8 @@ exports.contato_post = (req, res, next) => {
         
         from: c_mail,  // sender address
         to: process.env.MAIL_TO,   // list of receivers
-        subject: c_name+' - '+c_title,
-        text: c_message
+        subject: c_title,
+        text: 'From:\n'+c_name+'<'+c_mail+'>\nMessage:\n'+c_message
     };
 
     transporter.sendMail(mailData, (err, info) => {
@@ -39,7 +39,8 @@ exports.contato_post = (req, res, next) => {
         res.render('site_contato',
             { 
                 title: 'Contato', 
-                message: c_name+'. Sua mensagem foi enviada! Sua resposta será encaminhada o mais breve possível.' 
+                message: c_name+'. Sua mensagem foi enviada! Sua resposta será encaminhada o mais breve possível.',
+                csrfToken: req.csrfToken() 
             });
     });
 };
